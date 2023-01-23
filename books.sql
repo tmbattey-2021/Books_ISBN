92dd6775440249cafa1291a737a97be48f8f66f6
tmbattey-2021 <55719861+tmbattey-2021@users.noreply.github.com>
2023-01-23 18:54:45 -0500

    Add files via upload

---
+++ books.sql
@@ -0,0 +1,13 @@
+CREATE TABLE books(
+publisher varchar(30),
+image varchar(500),
+title_long varchar(125),
+edition char(3),
+authors1 varchar(30),
+authors2 varchar(30),
+title varchar(50),
+isbn13 char(13),
+binding varchar(29),
+isbn varchar(10),
+yr_published char(4)
+);
