a249ce3f65f5da559a01aae92b7a73d34db3cc42
tmbattey-2021 <55719861+tmbattey-2021@users.noreply.github.com>
2023-02-10 11:32:15 -0500

    Add files via upload

---
+++ book_server_final_0310.js
@@ -0,0 +1,158 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const url = require('url');
+const mysql = require('mysql2');
+
+const https = require('https');
+const hostname = '127.0.0.1';
+
+require('dotenv').config();
+
+
+const form = fs.readFileSync(path.join(__dirname, "public", "books.html"));
+
+
+const server = http.createServer((req, res) => {
+
+  var q = url.parse(req.url, true).query;
+  var txt = q.isbn;
+  var path = url.parse(req.url, true).pathname;
+ 
+  console.log(path);
+  console.log(txt);
+  var pathvar = "/book/" + txt;
+  console.log(pathvar);
+
+  if (path === "/") {
+    get(res);
+    return;
+  }
+
+ function get(res) {
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  res.end(form);
+}   
+
+  if (path === '/books') {
+
+      const options =
+      {
+          hostname: 'api2.isbndb.com',
+          port: 443,
+          path: pathvar,
+          headers: {
+              Authorization: '46273_9ea83635b87cfb9ab07eb849cc18f14a',
+              accept: 'application/json',
+          },
+          method: 'GET',
+      };
+  
+      const req = https.request(options, res => {
+          console.log(`statusCode: ${res.statusCode}`);
+          console.log(typeof(res.statusCode));
+
+       if (res.statusCode === 404)
+             {
+                console.log('404 error');
+               errorfunction();
+           //    res.writeHead(200, {'Content-Type': 'text/html'});
+             //   res.write("The book was successfully created!");
+             //   res.end(form);
+
+              }
+
+       // else
+
+      //  {
+
+        if (res.statusCode === 200)
+        {
+
+        
+          res.on('data', d => {
+              process.stdout.write(d);
+  
+              const obj = JSON.parse(d);
+              console.log(obj.book.publisher);
+              var $pub_var = obj.book.publisher;
+              var $image = obj.book.image;
+              var $title_long = obj.book.title_long;
+              var $edition = obj.book.edition;
+              var e = new Date(obj.book.date_published);
+              var year_var = (e.getUTCFullYear());
+              var str_year = year_var.toString();
+              var $authors1 = obj.book.authors[0];
+              var $authors2 = obj.book.authors[1];
+              var $isbn13 = obj.book.isbn13;
+              var $isbn = obj.book.isbn;
+              var $binding = obj.book.binding;
+  
+              post = { publisher: $pub_var, image: $image, title_long: $title_long, edition: $edition, authors1: $authors1, authors2: $authors2, isbn13: $isbn13, isbn: $isbn, binding: $binding, yr_published: str_year };
+  
+  
+  
+              const connection = mysql.createConnection({
+                 host: process.env.HOST_ID,
+                 database: process.env.DB_ID,
+                 user: process.env.USER_ID,
+                 password: process.env.USER_KEY,
+              });
+  
+              connection.connect();
+  
+  
+              connection.query(
+                  'INSERT INTO books SET ?', post, (err, results) => {
+                      if (err) console.log(err);
+                      console.log(results);
+  
+                  });
+  
+  
+  
+              connection.end();
+               myfunction();   
+                
+          });
+        }
+      });
+    
+      function errorfunction(){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("The isbn is incorrect.  Please retry.");
+      //    console.error(error);
+    //      res.write("The isbn is incorrect.  Please retry.");
+        res.end(form);
+
+      }
+
+    //  req.on('error', error => {
+     //   res.writeHead(200, {'Content-Type': 'text/html'});
+     //   res.write("The isbn is incorrect.  Please retry.");
+      //    console.error(error);
+    //      res.write("The isbn is incorrect.  Please retry.");
+      //  res.end(form);
+
+     // });
+  
+      req.end();
+  
+  };
+ 
+  function myfunction(){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The book was successfully created!");
+    res.end(form);
+
+  }
+  
+ }
+);
+
+server.listen(3000, hostname, () => {
+  console.log(`Server running at 3000`);
+});
